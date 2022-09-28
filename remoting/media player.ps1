03a83bbb3e4626d3b0303db66a4518137b66290d
George Boone <yongboone1@gmail.com>
2022-09-27 19:01:44 -0500

    unrelated junk
    remote desktop scheduled stupid event

--- remoting/media player.ps1
+++ remoting/media player.ps1
@@ -1,60 +1,15 @@
-﻿$scr
-$res = "C:\Users\Public\rmdResource.csv"
+﻿$filepath = "C:\Users\Owner\desktop\disappear.mp4"
 
-function get-vidTime {
-   param (
-        [Parameter (Mandatory = $true)] [string]$path
-    )
-    
-    $wmplayer = New-Object System.Windows.Media.MediaPlayer;
-    $wmplayer.Open($path);
-    Start-Sleep 2;
-    $t = $wmplayer.NaturalDuration.TimeSpan.Seconds;
-    $wmplayer.Close();
-    $t
-}
+#Here we use your code to get the duration of the video
+$wmplayer = New-Object System.Windows.Media.MediaPlayer
+$wmplayer.Open($filepath)
+Start-Sleep 2 
+$duration = $wmplayer.NaturalDuration.TimeSpan.Seconds
+$wmplayer.Close()
 
-function get-vids {
-   param (
-        [Parameter (Mandatory = $true)] [string]$path
-    )
-    
-    Get-ChildItem $path | ?{$_.Name -like "*.mp4" -or $_.Name -like "*.mp3" -or $_.Name -like "*.wav"} | %{$_.Name = $path + $_.Name}
-}
+#Here we just open media player and play the file, with an extra second for it to start playing
+$proc = Start-process -FilePath wmplayer.exe -ArgumentList $filepath -PassThru
+Start-Sleep ($duration + 1)
 
-function new-source{
-    param (
-        [Parameter (Mandatory = $true)] [string]$path
-    )
-
-    new-object psobject -Property @{
-        path = $path
-        time = (get-vidTime -path $path)
-        }
-}
-
-if([system.io.file]::Exists($res[0..($res.Length - 5)])){
-    `$scr = Import-Csv $res | Get-Random -Count 1
-} else {
-     $scr = ,([pscustomobject]@{path="filler"
-     time=0})
-     $scr | Export-Csv $res -NoTypeInformation
-     (Get-Item -Path $res -Force).Attributes='Hidden'
-     $scr = @()
-}
-
-if($scr.Count -eq 0){
-    $scr = (get-vids -path 'C:\Users\Public');
-    $scr | Export-Csv $res -NoTypeInformation
-}else {
-    if((get-vids -path 'C:\Users\Public').count -ne $scr.Count){
-        (get-vids -path 'C:\Users\Public') | ?{-not @($scr | select path).Contains($_)} | %{$v = new-source -path $_; $scr += $v; Add-Content $res ($v)}
-    }
-}
-$scr = $scr | Get-Random -Count 1
-
-$player = new-object -com WMPLAYER.OCX
-$player.openplayer($scr.path)
-
-Start-Sleep ($scr.time + 1)
-Stop-Process -Name "wmplayer" -Force
+#Here we kill the media player
+Stop-Process $proc.Id -force
